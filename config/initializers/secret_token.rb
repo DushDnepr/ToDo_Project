838d60364bd8b5a2e4a2cdb4a8f3167cab1a57f6
Andrey Igumnov <dushdnepr@gmail.com>
2012-01-17 10:46:25 +0200

    Empty ToDo project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todoproj::Application.config.secret_token = '8eb58b0dfaa6c3f8c8f4222efe8be3facae2db168a767fc3ac1860c2fd1aeb63572a6df996fd2434a89f71359bc75145b0af6ba9d82712cd6b44290aeaed8893'
